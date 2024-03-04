a228fd5931137efd89b7eaa0909abad0ef4a5e7c
goutham817 <158554789+goutham817@users.noreply.github.com>
2024-03-04 16:01:13 +0530

    changing the order of the script

--- backend.sh
+++ backend.sh
@@ -2,13 +2,13 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
+cp backend.service
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
-cp backend.service
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
