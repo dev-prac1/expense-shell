b96b29c71a85f2d1730aee04428d4dc58d11f2e3
goutham817 <158554789+goutham817@users.noreply.github.com>
2024-03-04 16:24:18 +0530

    adding expence.conf file

--- backend.sh
+++ backend.sh
@@ -14,4 +14,4 @@ systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
 dnf install mysql -y
-mysql -h <172.31.27.10> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h <172.31.15.69> -uroot -pExpenseApp@1 < /app/schema/backend.sql
